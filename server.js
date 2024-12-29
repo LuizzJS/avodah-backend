84569c1505508559f5b921f1bc63ee02f43d5f92
LuizzJS <luizz.developer@gmail.com>
2024-12-29 16:11:30 +0100

    Fixing Bugs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import express from "express";
 import authRoutes from "./routes/routes.js";
 import { connectDB } from "./database/connect.js";
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: ".env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
