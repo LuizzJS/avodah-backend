44f115a52d7f2eda89cf93007c3797408a18505c
LuizzJS <luizz.developer@gmail.com>
2025-01-01 15:14:23 +0100

    feat: implement user authentication check and cleanup unused code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,6 +7,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import path from "path";
 import axios from "axios";
+
 dotenv.config({ path: path.resolve(".env") });
 
 const roles = {
@@ -52,7 +53,6 @@ export const login = async (req, res) => {
     res.status(200).json({
       message: "Usuário logado com sucesso.",
       success: true,
-      token,
       data: {
         id: user._id,
         username: user.username,
@@ -68,14 +68,35 @@ export const login = async (req, res) => {
   }
 };
 
+export const isLogged = async (req, res) => {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(400).json({ success: false, message: "Not logged in" });
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(decoded.id);
+    res.status(200).json({
+      success: true,
+      message: "User is authenticated",
+      data: { id: user._id, username: user.username, email: user.email },
+    });
+  } catch (error) {
+    res
+      .status(401)
+      .json({ success: false, message: "Token inválido ou expirado." });
+  }
+};
+
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     if (!username || !email || !password)
-      return res.status(400).json({
-        message: "Todos os campos devem ser preenchidos.",
-        success: false,
-      });
+      return res
+        .status(400)
+        .json({
+          message: "Todos os campos devem ser preenchidos.",
+          success: false,
+        });
     const usernameExists = await User.findOne({
       username: username.toLowerCase(),
     });
@@ -93,33 +114,17 @@ export const register = async (req, res) => {
       rolePosition: roles["membro"],
     });
     await user.save();
-    res.status(201).json({
-      message: "Usuário criado com sucesso.",
-      success: true,
-      data: { ...user._doc, password: undefined },
-    });
+    res
+      .status(201)
+      .json({
+        message: "Usuário criado com sucesso.",
+        success: true,
+        data: { ...user._doc, password: undefined },
+      });
   } catch (error) {
     res
       .status(500)
       .json({ message: "Erro interno no servidor.", success: false });
-  }
-};
-
-export const isLogged = async (req, res) => {
-  const token = localStorage.getItem("token") || req.cookies.token;
-  if (!token) return res.status(400).json({ success: false });
-  try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const user = await User.findById(decoded.id);
-    res.status(200).json({
-      message: "Usuário autenticado com sucesso.",
-      success: true,
-      data: { ...user._doc, password: undefined },
-    });
-  } catch (error) {
-    res
-      .status(401)
-      .json({ message: "Token inválido ou expirado.", success: false });
   }
 };
 
@@ -130,8 +135,6 @@ export const logout = async (req, res) => {
       secure: true,
       sameSite: "strict",
     });
-    localStorage.removeItem("token");
-
     res
       .status(200)
       .json({ message: "Usuário deslogado com sucesso.", success: true });
@@ -139,161 +142,5 @@ export const logout = async (req, res) => {
     res
       .status(500)
       .json({ message: "Erro ao deslogar o usuário.", success: false });
-  }
-};
-
-export const setPassword = async (req, res) => {
-  const { password, email } = req.body;
-  try {
-    const user = await User.findOne({ email: email.toLowerCase() });
-    if (!user)
-      return res
-        .status(404)
-        .json({ message: "Usuário não encontrado.", success: false });
-    const newPassword = await bcrypt.hash(password, 10);
-    await User.updateOne(
-      { email: email.toLowerCase() },
-      { $set: { password: newPassword } }
-    );
-    res
-      .status(200)
-      .json({ message: "Senha atualizada com sucesso.", success: true });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Erro interno no servidor.", success: false });
-  }
-};
-
-export const setRole = async (req, res) => {
-  const token = localStorage.getItem("token") || req.cookies.token;
-  const { role, email } = req.body;
-  try {
-    if (!token)
-      return res
-        .status(401)
-        .json({ message: "Não autorizado.", success: false });
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const loggedUser = await User.findById(decoded.id);
-    if (loggedUser.rolePosition >= 4)
-      return res
-        .status(403)
-        .json({ message: "Usuário sem permissão.", success: false });
-    const user = await User.findOne({ email: email.toLowerCase() });
-    if (!user)
-      return res
-        .status(400)
-        .json({ message: "Usuário não encontrado.", success: false });
-    const normalizedRole = role?.toLowerCase();
-    if (!Object.keys(roles).includes(normalizedRole))
-      return res
-        .status(400)
-        .json({ message: "Cargo inválido.", success: false });
-    await User.updateOne(
-      { email: email.toLowerCase() },
-      { $set: { role: normalizedRole, rolePosition: roles[normalizedRole] } }
-    );
-    return res
-      .status(200)
-      .json({ message: "Cargo atualizado com sucesso.", success: true });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Erro interno no servidor.",
-      error: error.message,
-      success: false,
-    });
-  }
-};
-
-export const sendReport = async (req, res) => {
-  const { name, email, description } = req.body;
-  const client = new MailtrapClient({
-    token: "6a53c66ece2120782e097e5cfb94d353",
-  });
-  try {
-    await client.send({
-      from: { name: "Avodah Church", email: "mailtrap@demomailtrap.com" },
-      to: [{ email: "luizz.developer@gmail.com" }],
-      subject: "New Error Report!",
-      text: `Name: ${name}\nEmail: ${email}\nDescription: ${description}`,
-    });
-    res.status(200).send({ message: "Email sent successfully!" });
-  } catch (error) {
-    res.status(500).send({ error: "Failed to send email." });
-  }
-};
-
-export const getUserInfo = async (req, res) => {
-  const { id } = req.query;
-  try {
-    const user = await User.findOne({ username: id });
-    if (!user)
-      return res.status(404).json({ message: "Usuário não encontrado." });
-    return { ok: true, user: user };
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao buscar o usuário." });
-  }
-};
-
-export const sendPost = async (req, res) => {
-  const { title, content, author, authorId } = req.body;
-  const postId = uuidv4();
-  try {
-    const newPost = new Post({ title, content, author, authorId, postId });
-    await newPost.save();
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(500).json({ message: "Failed to create post" });
-  }
-};
-
-export const getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.find().sort({ createdAt: -1 });
-    res.status(200).json(posts);
-  } catch (error) {
-    res.status(500).json({ message: "Failed to fetch posts" });
-  }
-};
-
-export const getPost = async (req, res) => {
-  const postId = req.params.postId;
-  try {
-    const post = await Post.findOne({ postId: postId });
-    if (!post) return res.status(404).json({ message: "Post not found" });
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json({ message: "Failed to fetch post" });
-  }
-};
-
-export const removePost = async (req, res) => {
-  const postId = req.params.postId;
-  try {
-    const post = await Post.findOne({ postId: postId });
-    if (!post) return res.status(404).json({ message: "Post not found" });
-    await Post.deleteOne({ postId: postId });
-    res.status(200).json({ message: "Post deleted successfully", ok: true });
-  } catch (error) {
-    res.status(500).json({ message: "Failed to delete post" });
-  }
-};
-
-export const generateVerse = async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://bible-api.com/?random=verse&translation=almeida"
-    );
-    if (response.status !== 200)
-      res.status(400).json({
-        message: "Failed to generate verse.",
-        success: false,
-        data: null,
-      });
-
-    return res.status(200).json({ data: response.data, success: true });
-  } catch (error) {
-    console.error("Error generating verse:", error);
-    return { success: false, message: "Failed to generate verse." };
   }
 };
