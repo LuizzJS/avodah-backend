721f47e4d5379e1a9c649c867a17791f4b85ae4a
LuizzJS <luizz.developer@gmail.com>
2025-01-01 15:26:29 +0100

    fix: simplify dotenv configuration and update CORS options

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import { connectDB } from "./database/connect.js";
 import authRoutes from "./routes/routes.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 const corsOptions = {
   origin: "https://avodahsite.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
-  withCredentials: true,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -26,13 +22,6 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-  });
-}
 
 connectDB();
 
