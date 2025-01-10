df0eb8ad860b36ef21ce17967872bce31a6ac2b1
LuizzJS <luizz.developer@gmail.com>
2025-01-10 17:56:54 +0100

    fix: simplify database connection by removing unnecessary options

--- database/connect.js
+++ database/connect.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const database = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const database = await mongoose.connect(process.env.MONGO_URI);
 
     console.clear();
     console.log(
