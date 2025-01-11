8ad5f12f6564875afe1e393b1bd8ac01b58e444f
LuizzJS <luizz.developer@gmail.com>
2025-01-11 14:36:52 +0100

    fix: enhance CORS configuration by adding headers and handling preflight requests

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import authRoutes from "./routes/routes.js";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
@@ -18,7 +17,19 @@ const corsOptions = {
   credentials: true,
 };
 
-res.header("Access-Control-Allow-Origin", "https://avodahsite.vercel.app");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://avodahsite.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
