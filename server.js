8a0f8e60d3aec19b39d8e4e163430778a9a22cfa
LuizzJS <luizz.developer@gmail.com>
2025-01-11 14:41:46 +0100

    fix: update CORS configuration to allow localhost for development

--- server.js
+++ server.js
@@ -11,14 +11,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://avodahsite.vercel.app",
+  origin: ["https://avodahsite.vercel.app", "http://localhost:5173"],
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://avodahsite.vercel.app");
+  res.header("Access-Control-Allow-Origin", [
+    "https://avodahsite.vercel.app",
+    "http://localhost:5173",
+  ]);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
