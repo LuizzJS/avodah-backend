097cdb749ac4c444c4297fc70433abfb9031ab34
LuizzJS <luizz.developer@gmail.com>
2025-02-03 18:10:58 +0100

    refactor: update import statements for User and Post models to use named imports

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model.js";
-import Post from "../models/post.model.js";
+import { User } from "../models/user.model.js";
+import { Post } from "../models/post.model.js";
 import { MailtrapClient } from "mailtrap";
 import bcrypt from "bcryptjs";
 import { v4 as uuidv4 } from "uuid";
