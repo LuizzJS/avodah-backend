7907c2e4689b54b36c01306cbc8a94da90faa00b
LuizzJS <luizz.developer@gmail.com>
2025-02-03 18:08:07 +0100

    refactor: update import statements for User and Post models to improve clarity

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
-import { User, Post } from "../models/index.js";
+import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
 import { MailtrapClient } from "mailtrap";
 import bcrypt from "bcryptjs";
 import { v4 as uuidv4 } from "uuid";
