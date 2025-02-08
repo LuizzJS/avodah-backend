e0b98dcfd33aa2c947b3d3d8253a6b10a7d977ef
LuizzJS <luizz.developer@gmail.com>
2025-02-08 15:13:42 +0100

    fix: update cookie security setting based on environment

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -73,7 +73,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "Lax",
     });
 
